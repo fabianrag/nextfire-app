ebbb3f269917e746b8901fb1e74284b0845323a2
Fabian Araya <fbnarayaguerra@gmail.com>
2023-02-21 18:16:00 -0300

    Toast added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import NavBar from '@/components/Navbar'
 import '@/styles/globals.css'
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <NavBar />
       <Component {...pageProps} />
+      <Toaster />
     </>
   )
 }
