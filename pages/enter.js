28bec4513a14d4f535c7915449a3f40ab9f989d4
Fabian Araya <fbnarayaguerra@gmail.com>
2023-02-22 23:56:50 -0300

    google signin completed

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,39 @@
+import { auth, googleAuthProvider } from '@/lib/firebase'
+import { signInWithPopup, signOut } from 'firebase/auth'
+
 export default function EnterPage({}) {
+  const user = null
+  const username = null
+
   return (
     <main>
-      <h1>Sign up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   )
 }
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await signInWithPopup(auth, googleAuthProvider)
+  }
+
+  return (
+    <button className='btn-google' onClick={signInWithGoogle}>
+      <img src={'/google.png'} /> Sign in with Google
+    </button>
+  )
+}
+
+function SignOutButton() {
+  return <button onClick={() => signOut(auth)}>Sign Out</button>
+}
+
+function UsernameForm() {}
