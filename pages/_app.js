5f000765fdaa10de879744d8b15812817224551a
Fabian Araya <fbnarayaguerra@gmail.com>
2023-02-24 00:39:12 -0300

    auth hook completed

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import NavBar from '@/components/Navbar'
 import { UserContext } from '@/lib/context'
 import '@/styles/globals.css'
 import { Toaster } from 'react-hot-toast'
+import { useUserData } from '@/lib/hooks'
 
 export default function App({ Component, pageProps }) {
+  const userData = useUserData()
+
   return (
-    <UserContext.Provider value={{ user: {}, username: 'jeff' }}>
+    <UserContext.Provider value={userData}>
       <NavBar />
       <Component {...pageProps} />
       <Toaster />
