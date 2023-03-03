e0a95c2f9db1488e8dd5dbe90772ab2d05a63cf5
Fabian Araya <fbnarayaguerra@gmail.com>
2023-03-03 12:06:21 -0300

    ssr completed

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,3 +1,46 @@
-export default function UserProfilePage({}) {
-  return <main><h1>User Profile</h1></main>
+import UserProfile from '@/components/UserProfile'
+import PostFeed from '@/components/PostFeed'
+import { getUserWithUsername, postToJSON } from '@/lib/firebase'
+import {
+  collection,
+  query as query2,
+  limit,
+  orderBy,
+  where,
+  getDocs,
+} from 'firebase/firestore'
+
+export async function getServerSideProps({ query }) {
+  const { username } = query
+  const userDoc = await getUserWithUsername(username)
+
+  //JSON serializable data
+  let user = null
+  let posts = null
+
+  if (userDoc) {
+    user = userDoc.data()
+    const postsQuery = query2(
+      collection(userDoc.ref, 'posts'),
+      where('published', '==', true),
+      orderBy('createdAt', 'desc'),
+      limit(5)
+    )
+
+    const postsSnap = await getDocs(postsQuery)
+    posts = postsSnap.docs.map(postToJSON)
+  }
+
+  return {
+    props: { user, posts },
+  }
+}
+
+export default function UserProfilePage({ user, posts }) {
+  return (
+    <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+    </main>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,83 @@ import Link from 'next/link'
 import Loader from '../components/Loader'
 
 import toast from 'react-hot-toast'
+import {
+  collectionGroup,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+  where,
+} from 'firebase/firestore'
+import { useState } from 'react'
+import { db, fromMillis, postToJSON } from '@/lib/firebase'
+import PostFeed from '@/components/PostFeed'
 
-export default function Home() {
+//Max post to query per page
+const LIMIT = 1
+
+export async function getServerSideProps(context) {
+  const postsQuery = query(
+    collectionGroup(db, 'posts'),
+    where('published', '==', true),
+    orderBy('createdAt', 'desc'),
+    limit(LIMIT)
+  )
+
+  const postsSnap = await getDocs(postsQuery)
+  const posts = postsSnap.docs.map(postToJSON)
+
+  return {
+    props: { posts },
+  }
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts)
+  const [loading, setLoading] = useState(false)
+
+  const [postsEnd, setPostsEnd] = useState(false)
+
+  const getMorePosts = async () => {
+    setLoading(true)
+    const last = posts[posts.length - 1]
+
+    const cursor =
+      typeof last.createdAt === 'number'
+        ? fromMillis(last.createdAt)
+        : last.createdAt
+
+    const q = query(
+      collectionGroup(db, 'posts'),
+      where('published', '==', true),
+      orderBy('createdAt', 'desc'),
+      startAfter(cursor),
+      limit(LIMIT)
+    )
+
+    const postsSnap = await getDocs(q)
+    const newPosts = postsSnap.docs.map((doc) => doc.data())
+
+    setPosts(posts.concat(newPosts))
+    setLoading(false)
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true)
+    }
+  }
+
   return (
-    <div>
-      <button onClick={() => toast.success('hello toast!')}>Toast me</button>
-    </div>
+    <main>
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && (
+        <button onClick={getMorePosts}>Load more</button>
+      )}
+
+      <Loader show={loading} />
+
+      {postsEnd && 'You have reached the end!'}
+    </main>
   )
 }
